90967269ddc9790420f0d1dda83e36479807f92c
william bautista <wbautista@seedem.co>
2018-10-23 00:03:38 -0500

    Ajuste minimo

--- routes/hospital.js
+++ routes/hospital.js
@@ -9,7 +9,7 @@ var app = express();
 // ================================================
 
 app.get('/', (req, res) => {
-    Hospital.find({}, )
+    Hospital.find({})
         .exec((err, hospitals) => {
             if (err) {
                 return res.status(500).json({
